d460e39b6d17e03564924ef7e1ee23bc91cc427f
Maksim Blyznyuk <133545862+maksblyz@users.noreply.github.com>
2024-05-04 18:42:06 -0400

    Final? perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -7,9 +7,15 @@ np.set_printoptions(linewidth=np.inf)
 
 # Reads in the digit images from a given file such as 'trainingimages', and creates a 3D matrix where each 28x28
 # 2D matrix is one of the 28x28 images representing a digit
-def read_data_into_3d(file_path):
+def read_data_into_3d(file_path, input):
 
-    
+    if(input=="1"):
+        height = 70
+        weight = 60
+    else:
+        height = 28
+        weight = 28
+
 
     matrix_list = []
     current_matrix = []
@@ -22,19 +28,21 @@ def read_data_into_3d(file_path):
 
             if line == "":
                 if current_matrix:
-                    if len(current_matrix) == 28:
+                    if len(current_matrix) == height:
                         matrix_list.append(current_matrix)
                         current_matrix = []
                     else:
                         raise ValueError(f"Incomplete matrix at lines around {line_number - 28} to {line_number - 1}")
                 continue
 
-            if len(line) != 28:
+            if len(line) != weight:
+                print(weight)
+                print(len(line))
                 raise ValueError(
                     f"Line {line_number} does not contain exactly 28 characters (found {len(line)} characters)")
             current_matrix.append([char if char != '' else '' for char in line])
 
-            if len(current_matrix) == 28:
+            if len(current_matrix) == height:
                 matrix_list.append(current_matrix)
                 current_matrix = []
 
@@ -48,7 +56,11 @@ def read_data_into_3d(file_path):
 def assign_labels(file_path, type):
 
     n = 5000
-    if(type == 3):
+    if(type == 1):
+        n=451
+    elif(type == 2):
+        n=301
+    elif(type == 3):
         n=1000
 
     labels_dict = {}
@@ -64,6 +76,7 @@ def assign_labels(file_path, type):
             else:
                 raise ValueError("Non-integer value found in labels file")
     if len(labels_dict) != n:
+        print(len(labels_dict))
         raise ValueError("The number of labels does not match the expected count of 5000")
     return labels_dict
 
@@ -99,9 +112,6 @@ def perceptron(features, labels, alpha=1.0, max_iterations=1000):
         for i in range(num_samples):
             if labels[i] * np.dot(features[i], theta) <= 0:
                 Delta += labels[i] * features[i]  # Update delta if wrong
-                print(str(iteration) + " is incorrect")
-        if labels[i] * np.dot(features[i], theta) > 0:
-            print(str(iteration) + " is correct")
         Delta /= num_samples  # find average update
         theta += alpha * Delta  # Update weights
         iteration += 1
@@ -145,30 +155,134 @@ def convert_to_binary_labels(labels_dict):
         binary_labels.append(binary_label)
     return binary_labels
 
+user_input = input("Enter 0 for digit classification and 1 for facial recognition\n")
 
-training_matrix = read_data_into_3d("data/digitdata/trainingimages")
-training_labels_dict = assign_labels("data/digitdata/traininglabels", 0)
+if(user_input == "0"):
 
-binary_labels = convert_to_binary_labels(training_labels_dict)
+    training_matrix = read_data_into_3d("data/digitdata/trainingimages", user_input)
+    training_labels_dict = assign_labels("data/digitdata/traininglabels", 0)
 
-features = np.array([map_features(image) for image in training_matrix])
-labels = np.array(binary_labels)
+    binary_labels = convert_to_binary_labels(training_labels_dict)
 
-# Perceptron training
-weights = perceptron(features, labels, alpha=0.1, max_iterations=1000)
+    features = np.array([map_features(image) for image in training_matrix])
+    labels = np.array(binary_labels)
+
+    # Perceptron training
+    print("training...\n")
+    weights = perceptron(features, labels, alpha=0.1, max_iterations=1000)
+
+    testingInput = input("\nInput 0 for training set accuracy, 1 for validation set accuracy, or 2 for testing set accuracy\n")
+
+    if(testingInput == "0"):
+        accuracy = calculate_accuracy(features, labels, weights)
+        print(f"\nPerceptron Digit Characterization Accuracy (training): {accuracy:.2f}%\n")
+        np.savetxt("perceptron_digits_weights.txt", weights, fmt='%f')
+        print("Weights Succesfully Saved")
+
+    elif(testingInput == "1"):
+        validation_matrix = read_data_into_3d("data/digitdata/validationimages", user_input)
+        validation_labels_dict = assign_labels("data/digitdata/validationlabels", 3)
+
+        val_binary_labels = convert_to_binary_labels(validation_labels_dict)
+
+        val_features = np.array([map_features(image) for image in validation_matrix])
+        val_labels = np.array(val_binary_labels)
+
+        accuracy = calculate_accuracy(val_features, val_labels, weights)
+        print(f"\nPerceptron Digit Characterization Accuracy (training): {accuracy:.2f}%\n")
+
+        np.savetxt("perceptron_digit_weights.txt", weights, fmt='%f')
+        print("Weights Succesfully Saved")
+
+    elif(testingInput == "2"):
+        test_matrix = read_data_into_3d("data/digitdata/testimages", user_input)
+        test_labels_dict = assign_labels("data/digitdata/testlabels", 3)
+
+        test_binary_labels = convert_to_binary_labels(test_labels_dict)
+
+        test_features = np.array([map_features(image) for image in test_matrix])
+        test_labels = np.array(test_binary_labels)
+
+        accuracy = calculate_accuracy(test_features, test_labels, weights)
+        print(f"\nPerceptron Digit Characterization Accuracy (training): {accuracy:.2f}%\n")
+
+        np.savetxt("perceptron_digit_weights.txt", weights, fmt='%f')
+        print("Weights Succesfully Saved")
+
+    else: 
+        print("Invalid Input")
 
 
-validation_matrix = read_data_into_3d("data/digitdata/validationimages")
-validation_labels_dict = assign_labels("data/digitdata/validationlabels", 3)
+    validation_matrix = read_data_into_3d("data/digitdata/validationimages", user_input)
+    validation_labels_dict = assign_labels("data/digitdata/validationlabels", 3)
 
-val_binary_labels = convert_to_binary_labels(validation_labels_dict)
+    val_binary_labels = convert_to_binary_labels(validation_labels_dict)
 
-val_features = np.array([map_features(image) for image in validation_matrix])
-val_labels = np.array(val_binary_labels)
+    val_features = np.array([map_features(image) for image in validation_matrix])
+    val_labels = np.array(val_binary_labels)
 
 
-accuracy = calculate_accuracy(val_features, val_labels, weights)
-print(f"Accuracy: {accuracy:.2f}%")
+    accuracy = calculate_accuracy(val_features, val_labels, weights)
+    print(f"\nPerceptron Digit Classification Accuracy: {accuracy:.2f}%\n")
 
-np.savetxt("perceptron_weights.txt", weights, fmt='%f')
-print("weights saved")
+    np.savetxt("perceptron_digits_weights.txt", weights, fmt='%f')
+    print("Weights Succesfully Saved")
+
+elif(user_input == "1"):
+
+    training_matrix = read_data_into_3d("data/facedata/facedatatrain", user_input)
+    training_labels_dict = assign_labels("data/facedata/facedatatrainlabels", 1)
+
+    binary_labels = convert_to_binary_labels(training_labels_dict)
+
+    features = np.array([map_features(image) for image in training_matrix])
+    labels = np.array(binary_labels)
+
+    # Perceptron training
+    weights = perceptron(features, labels, alpha=0.1, max_iterations=1000)
+
+    testingInput = input("\nInput 0 for training set accuracy, 1 for validation set accuracy, or 2 for testing set\n")
+
+    if(testingInput == "0"):
+        accuracy = calculate_accuracy(features, labels, weights)
+        print(f"\nPerceptron Facial Recognition Accuracy (training): {accuracy:.2f}%\n")
+        np.savetxt("perceptron_faces_weights.txt", weights, fmt='%f')
+        print("Weights Succesfully Saved")
+
+    elif(testingInput == "1"):
+        validation_matrix = read_data_into_3d("data/facedata/facedatavalidation", user_input)
+        validation_labels_dict = assign_labels("data/facedata/facedatavalidationlabels", 2)
+
+        val_binary_labels = convert_to_binary_labels(validation_labels_dict)
+
+        val_features = np.array([map_features(image) for image in validation_matrix])
+        val_labels = np.array(val_binary_labels)
+
+        accuracy = calculate_accuracy(val_features, val_labels, weights)
+        print(f"\nPerceptron Facial Recognition Accuracy (validation): {accuracy:.2f}%\n")
+
+        np.savetxt("perceptron_faces_weights.txt", weights, fmt='%f')
+        print("Weights Succesfully Saved")
+
+    elif(testingInput == "2"):
+        test_matrix = read_data_into_3d("data/facedata/facedatatest", user_input)
+        test_labels_dict = assign_labels("data/facedata/facedatatestlabels", 2)
+
+        test_binary_labels = convert_to_binary_labels(test_labels_dict)
+
+        test_features = np.array([map_features(image) for image in test_matrix])
+        test_labels = np.array(test_binary_labels)
+
+        accuracy = calculate_accuracy(test_features, test_labels, weights)
+        print(f"\nPerceptron Facial Recognition Accuracy (test): {accuracy:.2f}%\n")
+
+        np.savetxt("perceptron_faces_weights.txt", weights, fmt='%f')
+        print("Weights Succesfully Saved")
+
+    else: 
+        print("Invalid Input")
+        
+else:
+    print("Invalid Input")
+
+
